3733cc973dea46b9087d4ca7b0c6f69717a6d72a
qaiik <82181231+qaiik@users.noreply.github.com>
2021-11-22 21:01:18 -0700

    Update barium.app.js

--- example/barium.app.js
+++ example/barium.app.js
@@ -1,2 +1,7 @@
 const plugins = require("plugins.config.js").config;
 const secrets = require("secrets.app.js");
+const { slice } = require("./barium/slices.js");
+
+let hello = slice("example");
+
+hello()
